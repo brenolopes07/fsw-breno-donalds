98c0bcf8fd120a85462419a6c7629c66a7b834e1
Breno Lopes <breno.skolegas07@gmail.com>
2025-02-19 01:29:32 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+    return(
+        <div>
+            <h1>Product Page</h1>
+            <Button>Fsw 7.0</Button>
+            <Input placeholder="email"/>
+        </div>
+    )
+
+    
+}
+ 
+export default ProductPage ;
